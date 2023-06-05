e1d7840483ae7ba72ced11f0396449522d7029db
1772012 <it1772012@student.it.maranatha.edu>
2023-06-05 16:08:48 +0700

    fix: bugs

--- resources/views/pages/applications/inc/btn-mail-application.blade.php
+++ resources/views/pages/applications/inc/btn-mail-application.blade.php
@@ -1,5 +1,5 @@
 {{-- Mail application button --}}
-@if ($model->is_processed)
+@if ($model->applicationStudyPrograms->first()->is_processed)
     <a class="btn btn-circle btn-mail-application text-primary" href="{{ route('applications.mail', ['application' => $model]) }}" role="button">
         <i class="fas fa-paper-plane"></i>
     </a>
